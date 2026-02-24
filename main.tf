a17407cc09bb554ef11af1959fd553edff78f7fc
sandeshit <thapasandesh56@gmail.com>
2026-02-24 15:42:45 +0545

    ACR: Create azure container registry.
    - Create azure container registry
    - Assign role for cluster authorization for image pull.
    - TODO: uncomment the config once admin access granted.

--- main.tf
+++ main.tf
@@ -35,4 +35,20 @@ resource "azurerm_kubernetes_cluster" "k8s" {
     network_plugin    = "kubenet"
     load_balancer_sku = "standard"
   }
+   tags = {
+    Environment = "Production"
+  }
 }
+# resource "azurerm_container_registry" "example" {
+#   name                = "containerRegistry9808snds"
+#   resource_group_name = azurerm_resource_group.rg.name
+#   location            = azurerm_resource_group.rg.location
+#   sku                 = "Premium"
+# }
+
+# resource "azurerm_role_assignment" "example" {
+#   principal_id                     = azurerm_kubernetes_cluster.k8s.kubelet_identity[0].object_id
+#   role_definition_name             = "AcrPull"
+#   scope                            = azurerm_container_registry.example.id
+#   skip_service_principal_aad_check = true
+# }

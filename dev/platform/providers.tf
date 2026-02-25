537ce4d3c5df2bf2946e088435301a3ec30b0170
sandeshit <thapasandesh56@gmail.com>
2026-02-25 23:32:08 +0545

    feat(argo): setup argocd.
    - pass provider from the platform.

--- dev/platform/providers.tf
+++ dev/platform/providers.tf
@@ -28,3 +28,18 @@ provider "azurerm" {
 
   subscription_id = var.subscription_id
 }
+
+# No race condition when configuring Helm provider using AKS module outputs in same Terraform run. 
+# Terraform builds a dependency graph first, 
+# Detects provider depends on module.aks, 
+# Ensures the AKS cluster is created before initializing the Helm provider and applying Helm resources.
+provider "helm" {
+  kubernetes = {
+    host                   = module.aks.host
+    username               = module.aks.cluster_username
+    password               = module.aks.cluster_password
+    client_certificate     = base64decode(module.aks.client_certificate)
+    client_key             = base64decode(module.aks.client_key)
+    cluster_ca_certificate = base64decode(module.aks.cluster_ca_certificate)
+  }
+}
--- modules/argocd/main.tf
+++ modules/argocd/main.tf
@@ -5,18 +5,4 @@ resource "helm_release" "argocd" {
   version          = var.argocd_version
   namespace        = var.argocd_namespace
   create_namespace = true
-
-  depends_on = [
-    azurerm_kubernetes_cluster.k8s
-  ]
-
-  values = [
-    yamlencode({
-      configs = {
-        params = {
-          "server.insecure" = "true" # argo-cd.readthedocs.io/en/latest/operator-manual/ingress/
-        }
-      }
-    })
-  ]
 }

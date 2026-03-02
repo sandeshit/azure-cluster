37aba13ac697e5f3177d7daeb9303e8dfe4e2547
sandeshit <thapasandesh56@gmail.com>
2026-03-02 19:25:21 +0545

    chore(platform): use the latest modules in platform

--- dev/platform/main.tf
+++ dev/platform/main.tf
@@ -8,14 +8,42 @@ data "terraform_remote_state" "core" {
     }
 }
 
+module "vnet" {
+  source = "../../modules/vnet"
+  resource_group_name = data.terraform_remote_state.core.outputs.resource_group_name
+  resource_group_location = data.terraform_remote_state.core.outputs.resource_group_location
+  vnet_address_space = ["10.0.0.0/16"]
+}
+
+module "waf" {
+  source = "../../modules/waf"
+  waf_name = "togglewaf619"
+  resource_group_name = data.terraform_remote_state.core.outputs.resource_group_name
+  resource_group_location = data.terraform_remote_state.core.outputs.resource_group_location
+}
+
+module "ag" {
+  source = "../../modules/ag"
+  ag_name = "toggleag619"
+  resource_group_name = data.terraform_remote_state.core.outputs.resource_group_name
+  resource_group_location = data.terraform_remote_state.core.outputs.resource_group_location
+  subnet_id = module.vnet.subnet_id_frontend
+  firewall_policy_id = module.waf.waf_id
+}
+
 module "aks" {
   source             = "../../modules/aks"
-  cluster_name       = var.cluster_name
-  cluster_dns_prefix = var.cluster_dns_prefix
+  cluster_name       = "toggle-cluster-619"
+  cluster_dns_prefix = "dns-toggle-619"
   resource_group_name = data.terraform_remote_state.core.outputs.resource_group_name
   resource_group_location = data.terraform_remote_state.core.outputs.resource_group_location
   acr_name = data.terraform_remote_state.core.outputs.acr_name
-  node_count = var.node_count
+  node_count = 3
+  vnet_subnet_id = module.vnet.subnet_id_backend
+  dns_service_ip = "10.1.0.10"
+  service_cidr      = "10.1.0.0/16"
+  gateway_id = module.ag.ag_id
+  resource_group_id = data.terraform_remote_state.core.outputs.resource_group_id
 }
 
 module "argocd" {
@@ -35,5 +63,5 @@ module "dns_record" {
   source = "../../modules/dns_record"
   resource_group_name = data.terraform_remote_state.core.outputs.resource_group_name
   zone_name = data.terraform_remote_state.core.outputs.zone_name
-  cname_record_target = module.aks.aks_ingress_fqdn
+  target_resource_id = module.ag.ag_public_ip_id
 }
